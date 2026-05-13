9bbbf7aa119dbf1eb9871f74fee0bab005416fee
nosewicz <joshnosewicz@gmail.com>
2026-05-13 16:32:58 -0400

    fixed card

--- app/utils/Card.js
+++ app/utils/Card.js
@@ -3,52 +3,57 @@
 import Image from "next/image";
 import clsx from "clsx";
 
-export default function Card({ card, faceUp = true }) {
-  const baseClassName =
-    "w-20 h-28 rounded shadow cursor-pointer select-none flex items-center justify-center";
-
 export default function Card({ card, faceUp = true, selected = false }) {
-  const cardFrameClass = "relative w-[clamp(3.25rem,12vw,5rem)] aspect-[2/3] rounded-md shadow-lg cursor-pointer select-none overflow-hidden ring-1 ring-black/20 bg-white";
+  const cardFrameClass =
+    "relative w-[clamp(3.25rem,12vw,5rem)] aspect-[2/3] rounded-md shadow-lg cursor-pointer select-none overflow-hidden ring-1 ring-black/20 bg-white";
 
   // Face-down scenario (you may not need it if your game is always face-up)
   if (!faceUp) {
     return (
-      (<div
-        className={`${cardFrameClass} bg-blue-900 text-white flex items-center justify-center`}
+      <div
+        className={clsx(
+          cardFrameClass,
+          "bg-blue-900 text-white flex items-center justify-center"
+        )}
       >
         <Image
           src="/cards/back.png"
           alt="Card Back"
           fill
           sizes="80px"
-          className="object-contain" />
-      </div>)
+          className="object-contain"
+        />
+      </div>
     );
   }
 
   if (card.rank === "Joker") {
     return (
-      (<div
-        className={`${cardFrameClass} bg-blue-900 text-white flex items-center justify-center`}
+      <div
+        className={clsx(
+          cardFrameClass,
+          "bg-blue-900 text-white flex items-center justify-center"
+        )}
       >
         <Image
           src="/cards/joker.png"
           alt="Joker"
           fill
           sizes="80px"
-          className="object-contain" />
-      </div>)
+          className="object-contain"
+        />
+      </div>
     );
   }
 
-  
-  const filename = `${card.rank}${card.suit}.png`; 
+  const filename = `${card.rank}${card.suit}.png`;
 
   return (
     <div
       className={clsx(
         cardFrameClass,
-        "flex flex-col items-center justify-center hover:-translate-y-1 transition"
+        "flex flex-col items-center justify-center hover:-translate-y-1 transition",
+        selected && "ring-2 ring-yellow-300"
       )}
     >
       <Image
@@ -56,7 +61,8 @@ export default function Card({ card, faceUp = true, selected = false }) {
         alt={`${card.rank} of ${card.suit}`}
         fill
         sizes="80px"
-        className="object-contain" />
-    </div>)
+        className="object-contain"
+      />
+    </div>
   );
 }
